62d1b8cc0b12ad392e3b719b203440bbee041dcc
Muhammad Umer Khan <um4r12@gmail.com>
2020-01-15 18:19:40 -0700

    completed client, echo server, and added functionality to accept multiple connections

--- client.py
+++ client.py
@@ -1,1 +1,46 @@
-import socket
+import socket, sys
+
+def create_tcp_socket():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except :
+        print("Failed to create socket")
+        sys.exit()
+    return s
+def send_data(serversocket, payload):
+    try:
+        serversocket.sendall(payload.encode())
+    except:
+        print("Send failed")
+        sys.exit()
+    print("Payload successfully sent.")
+
+def main():
+    try:
+        host = "www.google.com"
+        port = 80
+        payload = "GET / HTTP/1.0\r\nHost:" + host + "\r\n\r\n"
+        buffer_size = 4096
+
+        s = create_tcp_socket()
+
+        s.connect((host, port))
+
+        send_data(s, payload)
+        s.shutdown(socket.SHUT_WR)
+
+        full_data = b""
+
+        while True:
+            data = s.recv(buffer_size)
+            
+            if not data:
+                break
+            full_data += data
+        print(full_data)
+    except Exception as e:
+        print(e)
+    finally:
+        s.close()
+
+main()
